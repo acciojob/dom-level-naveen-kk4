8d5ddb3176d3d092f7fb77ae4c0d2837b8e84fdc
naveen-kk4 <133044450+naveen-kk4@users.noreply.github.com>
2023-08-22 17:37:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let element = document.getElementsByTagName("ul")[0];
+let children = element.children;
+let level = 0;
+for(let child of children){
+	level++;
+	if(child.id==="level")window.alert(level);
+}
